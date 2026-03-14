4cb80f9070c628aa601b7e098e339d720e875b79
PendurthiJyothsna <153272070+PendurthiJyothsna@users.noreply.github.com>
2026-03-14 12:50:21 +0530

    Add files via upload

---
+++ personal_info.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 12:16:55 2026
+
+@author: anany
+"""
+
+name=input("Enter the name:")
+age=int(input("Enter the age:"))
+hobby=input("Enter the Hobby:")
+print("What is your name?", name)
+print("How old are you?", age)
+print("What is your favorite hobby?", hobby)
+print(f"Welcome {name}!")
+print(f"You are {age} years old and love {hobby}.")
